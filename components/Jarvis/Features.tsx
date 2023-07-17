2bc8718fe5b77926fec88b13eed8400bbc632d94
myk <michaelmugendi1707@gmail.com>
2023-07-17 13:39:10 +0300

    Add: necessary components for jarvis ui and set specifications for expo and text to speeech

--- components/Jarvis/Features.tsx
+++ components/Jarvis/Features.tsx
@@ -1,10 +1,54 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 const Features = () => {
   return (
-    <View>
-      <Text>Features</Text>
+    <View className="w-full h-[60%] space-y-4">
+      <Text className="text-gray-700 font-semibold text-3xl">Features</Text>
+      <View className="bg-emerald-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            testID="chatgpt-icon"
+            source={require("../../assets/jarvis/chatgptIcon.png")}
+            className="w-10 h-10"
+          />
+          <Text className="font-semibold text-gray-700 text-lg">ChatGPT</Text>
+        </View>
+        <Text>
+          ChatGPT can provide you with knowledgeable responses, assists you with
+          creative ideas on a wide range of topics.
+        </Text>
+      </View>
+
+      <View className="bg-purple-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            testID="chatgpt-icon"
+            source={require("../../assets/jarvis/dalleIcon.png")}
+            className="w-10 h-10"
+          />
+          <Text className="font-semibold text-gray-700 text-lg">DALL-E</Text>
+        </View>
+        <Text>
+          DALL-E can generate imaginative and diverse images from textual
+          descriptions, expanding the boundaries of virtual creativity.
+        </Text>
+      </View>
+
+      <View className="bg-cyan-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            testID="chatgpt-icon"
+            source={require("../../assets/jarvis/smartaiIcon.png")}
+            className="w-10 h-10"
+          />
+          <Text className="font-semibold text-gray-700 text-lg">Smart AI</Text>
+        </View>
+        <Text>
+          A powerful voice assistant with abilities of ChatGPT and Dall-E,
+          providing you the best of both worlds.
+        </Text>
+      </View>
     </View>
   );
 };
--- screens/JarvisHomeScreen.tsx
+++ screens/JarvisHomeScreen.tsx
@@ -1,23 +1,158 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { TMessage, TMessages } from "../constants/JarvisTypes";
 import Features from "../components/Jarvis/Features";
+import { dummyMessages } from "../constants/dummyData";
+import Entypo from "@expo/vector-icons/Entypo";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const JarvisHomeScreen = () => {
-  const [messages, setMessages] = React.useState<TMessage[] | []>([]);
+  const [messages, setMessages] = React.useState<TMessage[] | []>(
+    dummyMessages
+  );
+  const [recording, setRecording] = React.useState(false);
+  const [speaking, setSpeaking] = React.useState(false);
+
+  const ClearMessages = () => {
+    setMessages([]);
+  };
+
+  const StopSpeaking = () => {
+    setSpeaking(false);
+  };
+
   return (
-    <SafeAreaView className="flex flex-1 mx-5 bg-white">
-      {/* bot icon */}
-      <View className="flex-row justify-center">
-        <Image
-          testID="bot-image"
-          source={require("../assets/jarvis/bot.png")}
-          className="w-[15%] h-[15%]"
-        />
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 mx-5">
+        {/* bot icon */}
+        <View className="flex-row justify-center">
+          <Image
+            testID="bot-image"
+            source={require("../assets/jarvis/bot.png")}
+            className="w-40 h-40"
+          />
+        </View>
+
+        {/* features || messages */}
+        {messages.length > 0 ? (
+          <View className="flex-1 space-y-2">
+            <Text className="text-gray-700 text-lg font-semibold ml-1">
+              Assistant
+            </Text>
+            <View className="h-[80%] bg-neutral-200 rounded-3xl p-4">
+              <ScrollView
+                className="space-y-4"
+                bounces={false}
+                showsVerticalScrollIndicator={false}
+              >
+                {messages.map((message, index) => {
+                  if (message.role === "assistant") {
+                    if (message.content.includes("https")) {
+                      //its an AI image
+                      return (
+                        <View
+                          testID="response-image"
+                          key={index}
+                          className="flex-row justify-start"
+                        >
+                          <View className=" bg-emerald-100 rounded-2xl p-2 rounded-tl-none">
+                            <Image
+                              source={{ uri: message.content }}
+                              className="w-48 h-48 rounded-2xl"
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      //text response
+                      return (
+                        <View
+                          key={index}
+                          testID="response-text"
+                          className="flex-row justify-start"
+                        >
+                          <View className="w-[70%] bg-emerald-100 rounded-xl p-2 rounded-tl-none italic">
+                            <Text className="">{message.content}</Text>
+                          </View>
+                        </View>
+                      );
+                    }
+                  } else {
+                    //user
+                    return (
+                      <View
+                        key={index}
+                        testID="user-request-text"
+                        className="flex-row justify-end"
+                      >
+                        <View className="w-[70%] bg-white rounded-xl p-2 rounded-tr-none">
+                          <Text className="">{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
       </View>
 
-      {/* features || messages */}
-      {messages.length > 0 ? <View></View> : <Features />}
+      {/* recording, clear and stop speech */}
+      <View className="flex-row items-center justify-center mb-4">
+        {recording ? (
+          <TouchableOpacity>
+            <Image
+              source={require("../assets/jarvis/voiceLoading.gif")}
+              className="w-20 h-20 rounded-full"
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity>
+            <Image
+              source={require("../assets/jarvis/recordingIcon.png")}
+              className="w-20 h-20 rounded-full"
+            />
+          </TouchableOpacity>
+        )}
+
+        {messages.length > 0 ? (
+          <TouchableOpacity
+            onPress={() => ClearMessages()}
+            className="bg-neutral-400 p-3 rounded-full absolute right-10 "
+          >
+            <MaterialIcons
+              testID="clear-btn"
+              name={"clear"}
+              size={30}
+              color={"black"}
+            />
+          </TouchableOpacity>
+        ) : null}
+
+        {speaking ? (
+          <TouchableOpacity
+            onPress={() => StopSpeaking()}
+            className="bg-neutral-400 p-3 rounded-full absolute left-10 "
+          >
+            <Entypo
+              testID="stop-btn"
+              name={"controller-stop"}
+              size={30}
+              color={"#ed544a"}
+            />
+          </TouchableOpacity>
+        ) : null}
+      </View>
     </SafeAreaView>
   );
 };
