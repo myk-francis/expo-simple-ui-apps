b39ce2ff64fc5aa56ca8ee8ed7fe92edc4fcd740
myk <michaelmugendi1707@gmail.com>
2023-07-19 10:32:13 +0300

    Add OnBoardingHomeScreen functionality and update lottie file confetti

--- screens/OnBoardingHomeScreen.tsx
+++ screens/OnBoardingHomeScreen.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
+import { View, SafeAreaView, Text } from "react-native";
 import React from "react";
+import LottieView from "lottie-react-native";
 
 const OnBoardingHomeScreen = () => {
+  const ref = React.useRef<LottieView | null>(null);
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current?.play();
+    }
+  }, [ref.current]);
+
   return (
-    <View>
-      <Text>OnBoardingHomeScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white items-center">
+      <LottieView
+        ref={ref}
+        autoPlay={false}
+        loop={false}
+        style={{
+          width: 200,
+          height: 300,
+          // backgroundColor: "#fff",
+        }}
+        source={require("../assets/onboarding/confetti1.json")}
+      />
+      <Text className="text-3xl mb-4">Home Page</Text>
+    </SafeAreaView>
   );
 };
 
--- screens/OnBoardingScreen.tsx
+++ screens/OnBoardingScreen.tsx
@@ -1,41 +1,97 @@
-import { View, Text } from "react-native";
+import { View, Text, Dimensions, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../navigation/AppStack";
+import { StackNavigationProp } from "@react-navigation/stack";
+
+const { width, height } = Dimensions.get("window");
+
+type TOnBoardingHomeScreenProp = StackNavigationProp<
+  RootStackParamList,
+  "OnBoardingHomeScreen"
+>;
+
+const DoneButton = ({ ...props }) => {
+  return (
+    <TouchableOpacity {...props} className="p-4 bg-white rounded-l-full">
+      <Text>Done</Text>
+    </TouchableOpacity>
+  );
+};
 
 const OnBoardingScreen = () => {
+  const navigation = useNavigation<TOnBoardingHomeScreenProp>();
+
+  const handleDone = () => {
+    navigation.navigate("OnBoardingHomeScreen");
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <Onboarding
+        bottomBarHighlight={false}
+        onDone={() => handleDone()}
+        onSkip={() => handleDone()}
+        // DoneButtonComponent={() => <DoneButton />}
         containerStyles={{
           paddingHorizontal: 15,
         }}
         pages={[
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#a7f3d0",
             image: (
               <View>
-                <Text>Onboarding 1</Text>
+                <LottieView
+                  autoPlay
+                  loop
+                  style={{
+                    width: 200,
+                    height: 300,
+                    // backgroundColor: "#fff",
+                  }}
+                  source={require("../assets/onboarding/boost.json")}
+                />
               </View>
             ),
             title: "Boost Productivity",
             subtitle: "We are glad you are here.",
           },
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#fef3c7",
             image: (
               <View>
-                <Text>Onboarding 2</Text>
+                <LottieView
+                  autoPlay
+                  loop
+                  style={{
+                    width: 200,
+                    height: 300,
+                    // backgroundColor: "#fff",
+                  }}
+                  source={require("../assets/onboarding/work.json")}
+                />
               </View>
             ),
             title: "Work Seamlessly",
             subtitle: "Get your work done seamlessly without intruptions.",
           },
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#a78bfa",
             image: (
               <View>
-                <Text>Onboarding 3</Text>
+                <LottieView
+                  autoPlay
+                  loop
+                  style={{
+                    width: 200,
+                    height: 300,
+                    // backgroundColor: "#fff",
+                  }}
+                  source={require("../assets/onboarding/achieve.json")}
+                />
               </View>
             ),
             title: "Achive Higher Goals",
