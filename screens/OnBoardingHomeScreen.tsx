355d02cbc228a2e99e1ca11d2c283aad377dfa93
myk <michaelmugendi1707@gmail.com>
2023-07-19 09:17:35 +0300

    Add: OnBoarding Screen and its Home screen

---
+++ screens/OnBoardingHomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const OnBoardingHomeScreen = () => {
+  return (
+    <View>
+      <Text>OnBoardingHomeScreen</Text>
+    </View>
+  );
+};
+
+export default OnBoardingHomeScreen;
