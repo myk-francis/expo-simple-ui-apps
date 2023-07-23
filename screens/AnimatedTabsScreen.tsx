69ccd83df23bbd52d82d9bd0331ccc9ce6f4f1e2
myk <michaelmugendi1707@gmail.com>
2023-07-23 15:09:28 +0300

    Animated tabs finished

--- screens/AnimatedTabsScreen.tsx
+++ screens/AnimatedTabsScreen.tsx
@@ -7,6 +7,9 @@ import {
   Dimensions,
   Animated,
   FlatList,
+  Image,
+  findNodeHandle,
+  TouchableOpacity,
 } from "react-native";
 
 const { width, height } = Dimensions.get("window");
@@ -25,26 +28,153 @@ const data = Object.keys(images).map((i) => ({
   key: i,
   title: i,
   image: images[i],
+  ref: React.createRef<any>(),
 }));
 
+const Tab = React.forwardRef(
+  ({ item, scrollX, onItempress }: any, ref: any) => {
+    return (
+      <TouchableOpacity onPress={onItempress}>
+        <View ref={ref}>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 84 / data.length,
+              fontWeight: "bold",
+              textTransform: "uppercase",
+            }}
+          >
+            {item.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+);
+
+const Indicator = ({ measures, scrollX }: any) => {
+  const inputRange = data.map((_, i) => i * width);
+  const indicatorWidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure: any) => measure.width),
+  });
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure: any) => measure.x),
+  });
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        height: 4,
+        width: indicatorWidth,
+        backgroundColor: "white",
+        bottom: -10,
+        transform: [{ translateX }],
+      }}
+    ></Animated.View>
+  );
+};
+
+const Tabs = ({ scrollX, onItempress }: any) => {
+  const [measures, setMeasures] = React.useState([]);
+  const containerRef = React.useRef<any>();
+
+  React.useEffect(() => {
+    let m: any = [];
+    data.forEach((item) => {
+      item?.ref?.current?.measureLayout(
+        containerRef.current,
+        (x: any, y: any, width: any, height: any) => {
+          // console.log(x, y, width, height);
+          m.push({
+            x,
+            y,
+            width,
+            height,
+          });
+
+          if (m.length === data.length) {
+            setMeasures(m);
+          }
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <View style={{ position: "absolute", top: 100, width }}>
+      <View
+        ref={containerRef}
+        style={{
+          justifyContent: "space-evenly",
+          flex: 1,
+          flexDirection: "row",
+        }}
+      >
+        {data.map((item, index) => {
+          return (
+            <Tab
+              item={item}
+              scrollX={scrollX}
+              key={item.key}
+              ref={item.ref}
+              onItempress={() => onItempress(index)}
+            />
+          );
+        })}
+      </View>
+      {measures?.length > 0 ? (
+        <Indicator measures={measures} scrollX={scrollX} />
+      ) : null}
+    </View>
+  );
+};
+
 const AnimatedTabs = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const ref = React.useRef<any>();
+  const onItempress = React.useCallback((itemIndex: any) => {
+    ref?.current?.scrollToOffset({
+      offset: itemIndex * width,
+      animated: true,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Text style={{ fontSize: 42 }}>❤️</Text>
-      <Text
-        style={{
-          fontFamily: "Menlo",
-          marginTop: 10,
-          fontWeight: "800",
-          fontSize: 16,
+      <Animated.FlatList
+        ref={ref}
+        data={data}
+        keyExtractor={(item) => item.key}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        bounces={false}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={{ width, height }}>
+              <Image
+                source={{ uri: item.image }}
+                style={{ flex: 1, resizeMode: "cover" }}
+              />
+              <View
+                style={[
+                  StyleSheet.absoluteFillObject,
+                  { backgroundColor: "rgba(0, 0, 0, 0.3)" },
+                ]}
+              ></View>
+            </View>
+          );
         }}
-      >
-        Expo
-      </Text>
-      <Text style={{ fontFamily: "Menlo", fontStyle: "italic", fontSize: 12 }}>
-        (expo.io)
-      </Text>
+      />
+
+      <Tabs scrollX={scrollX} onItempress={onItempress} />
     </View>
   );
 };
